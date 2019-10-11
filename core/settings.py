adc102ea1ea146f8e5c846a2a3ec5aa5976eca3a
dx <dx@dx.dx>
2019-10-11 13:03:06 +0330

    Add indexers path

--- core/settings.py
+++ core/settings.py
@@ -1,1 +1,5 @@
 BOOKS_DATABASE_PATH = "../resources/data/books/books.json"
+PUBLISHERS_DATABASE_PATH = "../resources/data/publishers/publishers.json"
+BOOK_AUTHOR_INDEX = "../resources/data/indexes/BookAuthor.json"
+BOOK_ISBN_INDEX = "../resources/data/indexes/BookISBN.json"
+BOOK_NAME_INDEX = "../resources/data/indexes/BookTitle.json"
