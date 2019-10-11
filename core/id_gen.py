3cb7584369874c3e23b18adf57b9a31aed715973
dx <dx@dx.dx>
2019-10-11 22:32:25 +0330

    Remove id for publisher

--- core/id_gen.py
+++ core/id_gen.py
@@ -12,15 +12,5 @@ def get_book_id():
         json.dump(book_id_data, book_id_file)
     return usable_id
 
-def get_publisher_id():
-    with open(PUBLISHERS_ID_TABLE, "r") as publisher_id_file:
-        publisher_id_data = json.load(publisher_id_file)
-        publisher_id_data["CurrentID"] += 1
-        usable_id = publisher_id_data["CurrentID"]
-    with open(PUBLISHERS_ID_TABLE, "w") as publisher_id_file:
-        json.dump(publisher_id_data, publisher_id_file)
-    return usable_id
-
 if __name__ == "__main__":
     print(get_book_id())
-    print(get_publisher_id())
